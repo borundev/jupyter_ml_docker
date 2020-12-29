dea392186cc6c390c0ea837c2d4a7db577a87117
borundev <borundev@gmail.com>
2020-12-29 01:47:26 +0100

    major refactoring of docker files

--- source_environment_variables.sh
+++ source_environment_variables.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
 export NB_UID=$(id -u)
 export NB_GID=$(id -g)
 export NB_USER=$(whoami)
 export HOSTNAME=$(hostname)
+
+export ML_HTTP_USER=user
+export ML_HTTP_AUTH_PASS_ENC="\$apr1\$0rh0Tkr3\$ALeFxLzN7Q7m7fvaBFB8H0"
 
 if [ -z ${NOTEBOOKS_FOLDER+x} ]; then echo "NOTEBOOKS_FOLDER is unset"; else echo "NOTEBOOKS_FOLDER is set to '$NOTEBOOKS_FOLDER'"; fi
