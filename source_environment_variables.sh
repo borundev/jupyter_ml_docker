f099d6e5bea273da4496c699cd0b0b5edf8b1719
borundev <borundev@gmail.com>
2020-12-30 19:28:19 +0100

    made the hostname dynamically decided

--- source_environment_variables.sh
+++ source_environment_variables.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
 export NB_UID=$(id -u)
 export NB_GID=$(id -g)
 export NB_USER=$(whoami)
-export HOSTNAME=$(hostname)
+if [ -z ${HOSTNAME+x} ]
+then 
+	export HOSTNAME=$(hostname)
+	echo "SETTING HOSTNAME = '$HOSTNAME'"
+else 
+	echo "HOSTNAME = '$HOSTNAME'"
+fi
 
 export ML_HTTP_USER=user
 export ML_HTTP_AUTH_PASS_ENC="\$apr1\$0rh0Tkr3\$ALeFxLzN7Q7m7fvaBFB8H0"
