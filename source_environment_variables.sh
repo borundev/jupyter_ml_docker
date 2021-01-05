6e8b683b578af004cb518f7d82fb94f1e409d926
borundev <borundev@gmail.com>
2021-01-05 21:55:10 +0100

    changed to image starting from ubuntu

--- source_environment_variables.sh
+++ source_environment_variables.sh
@@ -11,6 +11,12 @@ else
 fi
 
 export ML_HTTP_USER=user
-export ML_HTTP_AUTH_PASS_ENC="\$apr1\$0rh0Tkr3\$ALeFxLzN7Q7m7fvaBFB8H0"
+export ML_HTTP_AUTH_PASS_ENC="\$apr1\$8WVsyj71\$PYI615e5zi40cmlzPvph6."
+export MINIO_ACCESS_KEY=minio
+export MINIO_SECRET_KEY=minio@fey9ma9
+export MINIO_DIR=$HOME/minio/
+export MINIO_HTTP_USER=user
+export MINIO_HTTP_AUTH_PASS_ENC="\$apr1\$8WVsyj71\$PYI615e5zi40cmlzPvph6."
+
 
 if [ -z ${NOTEBOOKS_FOLDER+x} ]; then echo "NOTEBOOKS_FOLDER is unset"; else echo "NOTEBOOKS_FOLDER is set to '$NOTEBOOKS_FOLDER'"; fi
