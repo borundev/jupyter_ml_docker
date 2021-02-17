deedede66b39e5c5f98f0fc51707a021611f4661
borundev <borundev@gmail.com>
2021-02-17 14:43:51 +0100

    Refactored

--- source_environment_variables.sh
+++ source_environment_variables.sh
@@ -10,7 +10,5 @@ else
 	echo "HOSTNAME = '$HOSTNAME'"
 fi
 
-export ML_HTTP_USER= USERNAME
-export ML_HTTP_AUTH_PASS_ENC= ENCRYPTED_PASSWORD
 
 if [ -z ${NOTEBOOKS_FOLDER+x} ]; then echo "NOTEBOOKS_FOLDER is unset"; else echo "NOTEBOOKS_FOLDER is set to '$NOTEBOOKS_FOLDER'"; fi
